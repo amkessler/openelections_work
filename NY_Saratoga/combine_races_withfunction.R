506b210734b406eb0c3a58253406baa0f0dd023c
Kessler <ak113714@bna.com>
2021-01-03 17:37:22 -0500

    change refs to saratoga

--- NY_Saratoga/combine_races_withfunction.R
+++ NY_Saratoga/combine_races_withfunction.R
@@ -1,71 +1,46 @@
 library(tidyverse)
 library(janitor)
 library(readxl)
+library(arrow)
 
+#source processing function
+source("process_ny_data_functions.R")
 
-# BUILD FUNCTION ####
-
-### We'll create a function to handle the repetitive part of the processing
-# once the initial excel cleanup steps are taken. The file will have a precinct column,
-# followed by columns with vote results with names formatted as "candidate - party"
 # Function wants:
+# - dataset (or import from excel function)
 # - office: text label for office (e.g. "U.S. House")
 # - district: text label for district (e.g. "42")
-                                 
-process_ny_data <- function(df, office, district){
-  #determine how many columns, since races can have diff number of candidates
-  colnum <- length(colnames(df))  
-  #begin processing dataset
-  df <- df %>% 
-    #transform to long/tidy format
-    pivot_longer(cols = 2:all_of(colnum), names_to = "name", values_to = "votes") %>% 
-    #clean and add necessary columns
-    mutate(
-      temp = str_split(name, " - ", simplify = TRUE),
-      candidate = temp[, 1],
-      party = temp[, 2],
-      office = office,
-      district = district,
-      candidate = str_replace_all(candidate, "WriteIn", "Write-Ins") #standarize with openelex name
-    ) %>%
-    #reorder columns to match openelex format
-    select(
-      precinct, office, district, candidate, party, votes
-    ) 
-  #return results
-  return(df)
-}
-
+                    
 
 ## PROCESS DATA FILES ####
 
 # Presidential ####
 #run import and processing function in one step 
-processed_prez <- process_ny_data(read_excel("NY_Wyoming/Wyoming_NY_GE20_cleaned.xlsx", sheet = "presidential"), 
+processed_prez <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "presidential"), 
                                   "President", 
                                   "")
 processed_prez
 
 
-## Congressional - District 27 ####
-processed_cd27 <- process_ny_data(read_excel("NY_Wyoming/Wyoming_NY_GE20_cleaned.xlsx", sheet = "cd27"), 
+## Congressional - District 21 ####
+processed_cd21 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "cd21"), 
                                   "U.S. House", 
-                                  "27")
-processed_cd27
+                                  "21")
+processed_cd21
 
 
-## State Senate 59 ####
-processed_statesen59 <- process_ny_data(read_excel("NY_Wyoming/Wyoming_NY_GE20_cleaned.xlsx", sheet = "statesen59"),
+## State Senate 45 ####
+processed_statesen47 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statesen47"),
                                         "State Senate", 
-                                        "59")
-processed_statesen59
+                                        "47")
+processed_statesen47
 
 
 ## State House 114 ####
-processed_statehou147 <- process_ny_data(read_excel("NY_Wyoming/Wyoming_NY_GE20_cleaned.xlsx", sheet = "statehou147"),
+processed_statehou117 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statehou117"),
                                          "State Assembly", 
-                                         "147")
-processed_statehou147
+                                         "117")
+processed_statehou117
 
 
 
@@ -73,15 +48,15 @@ processed_statehou147
 
 #combine tidy/long datasets created above 
 processed_combined <- bind_rows(processed_prez,
-                                processed_cd27, 
-                                processed_statesen59, 
-                                processed_statehou147)
+                                processed_cd21, 
+                                processed_statesen47, 
+                                processed_statehou117)
 
 
 #add county name for all records ####
 processed_combined <- processed_combined %>% 
   mutate(
-    county = "Wyoming"
+    county = "Saratoga"
   ) %>% 
   select(county, everything())
 
@@ -104,4 +79,6 @@ processed_combined %>%
 ### EXPORT RESULTS ####
 
 #use openelex naming convention
-write_csv(processed_combined, "NY_Wyoming/20201103__ny__general__wyoming__precinct.csv", na = "")
+write_csv(processed_combined, "NY_Lewis/20201103__ny__general__saratoga__precinct.csv", na = "")
+arrow::write_feather(processed_combined, "NY_Lewis/20201103__ny__general__saratoga__precinct.feather")
+
