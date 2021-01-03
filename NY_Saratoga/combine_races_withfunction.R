1a03a2246a1e077808eeb7725fbdc0ef1f42985c
Kessler <ak113714@bna.com>
2021-01-03 17:45:25 -0500

    completed saratoga processing

--- NY_Saratoga/combine_races_withfunction.R
+++ NY_Saratoga/combine_races_withfunction.R
@@ -22,6 +22,13 @@ processed_prez <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_clean
 processed_prez
 
 
+## Congressional - District 20 ####
+processed_cd20 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "cd20"), 
+                                  "U.S. House", 
+                                  "20")
+processed_cd20
+
+
 ## Congressional - District 21 ####
 processed_cd21 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "cd21"), 
                                   "U.S. House", 
@@ -29,18 +36,50 @@ processed_cd21 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_clean
 processed_cd21
 
 
-## State Senate 45 ####
-processed_statesen47 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statesen47"),
+
+## State Senate 43 ####
+processed_statesen43 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statesen43"),
                                         "State Senate", 
-                                        "47")
-processed_statesen47
+                                        "43")
+processed_statesen43
+
+
+## State Senate 49 ####
+processed_statesen49 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statesen49"),
+                                        "State Senate", 
+                                        "49")
+processed_statesen49
+
+
+
+## State House 108 ####
+processed_statehou108 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statehou108"),
+                                         "State Assembly", 
+                                         "108")
+processed_statehou108
+
+
+## State House 112 ####
+processed_statehou112 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statehou112"),
+                                         "State Assembly", 
+                                         "112")
+processed_statehou112
+
+
+## State House 113 ####
+processed_statehou113 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statehou113"),
+                                         "State Assembly", 
+                                         "113")
+processed_statehou113
 
 
 ## State House 114 ####
-processed_statehou117 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statehou117"),
+processed_statehou114 <- process_ny_data(read_excel("NY_Saratoga/Saratoga_NY_GE20_cleaned.xlsx", sheet = "statehou114"),
                                          "State Assembly", 
-                                         "117")
-processed_statehou117
+                                         "114")
+processed_statehou114
+
+
 
 
 
@@ -48,9 +87,14 @@ processed_statehou117
 
 #combine tidy/long datasets created above 
 processed_combined <- bind_rows(processed_prez,
+                                processed_cd20,
                                 processed_cd21, 
-                                processed_statesen47, 
-                                processed_statehou117)
+                                processed_statesen43,
+                                processed_statesen49,
+                                processed_statehou108,
+                                processed_statehou112,
+                                processed_statehou113,
+                                processed_statehou114)
 
 
 #add county name for all records ####
